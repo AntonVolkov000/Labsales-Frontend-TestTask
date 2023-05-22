cd6505f529c33fe36fec45812c30b821c3b5d41c
Anton Volkov <xzxz2020.ru@mail.ru>
2023-05-23 00:10:12 +0500

    Added description and request sections

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// new Swiper('.image-slider', {
+//   speed: 0,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev'
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   touchRatio: 0,
+//   slideToClickedSlide: true,
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: true,
+//   },
+//   spaceBetween: 20,
+// });
